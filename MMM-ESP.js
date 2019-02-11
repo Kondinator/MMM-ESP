293b0eaa4d8468c4bcdd4d2d53c9c9dbab2f4f17
Kondinator <Kondiersej@live.dk>
2019-02-11 16:18:12 +0100

    yeee

--- MMM-ESP.js
+++ MMM-ESP.js
@@ -1,28 +1,38 @@
 //helloworld.js:
 
-Module.register("MMM-ESP",{
-	// Default module config.
-	defaults: {
-		text: "hello world" + data,
-	},
+Module.register("MMM-ESP", {
+  // Default module config.
+  defaults: {
+    text: "there should be some data around here"
+  },
 
-	// Override dom generator.
+  setPosition: function(pos) {
+		//Add css to body depending on the set position for notifications
+		var sheet = document.createElement("style");
+		if (pos === "center") {sheet.innerHTML = ".ns-box {margin-left: auto; margin-right: auto;text-align: center;}";}
+		if (pos === "right") {sheet.innerHTML = ".ns-box {margin-left: auto;text-align: right;}";}
+		if (pos === "left") {sheet.innerHTML = ".ns-box {margin-right: auto;text-align: left;}";}
+    document.body.appendChild(sheet);
+    
+  },
+
+  
+
+  // Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
 		wrapper.innerHTML = this.config.text;
 		return wrapper;
-    },
-    
-    getResp: function(){
-        
-        $:ajax({
-        url:"http://10.10.10.166",
-        type: 'GET',
-        dataType: 'jsonp',
-        success : function(data){
-        console.log(data)
-        }
-      })
-    }
-	
+	},
+
+  getData() {
+
+
+    var response, xmlhttp;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://10.10.10.166", false);
+    xhttp.send()
+    var celsius = xhttp.responseText;
+    document.getElementById("test").innerHTML = "bruh shit works " + celsius;
+  }
 });
