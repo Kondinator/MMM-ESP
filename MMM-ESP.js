d3a7ca8107a2a36536c90fdae34b9ad9b4bdc31d
Kondinator <Kondiersej@live.dk>
2019-02-12 13:15:27 +0100

    yaahyeeeeet

--- MMM-ESP.js
+++ MMM-ESP.js
@@ -5,7 +5,7 @@
 Module.register("MMM-ESP", {
   // Default module config.
   defaults: {
-    starttext: "there should be some data around here",
+    firsttext: "there should be some data around here",
   },
 
 
@@ -24,7 +24,7 @@ Module.register("MMM-ESP", {
     var celsius = xhttp.responseText;
     var element = document.createElement("div")
     element.className = "myContent"
-    element.innerHTML = celsius + " " + this.config.starttext
+    element.innerHTML = this.config.firsttext + " " + celsius + " " + this.config.lasttext
     return element
 
   },
