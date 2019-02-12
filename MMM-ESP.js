daf4cd20fe0d0272880c2a53a647df44987c4962
Kondinator <Kondiersej@live.dk>
2019-02-12 10:45:51 +0100

    sas

--- MMM-ESP.js
+++ MMM-ESP.js
@@ -5,15 +5,33 @@ var celsius;
 Module.register("MMM-ESP", {
   // Default module config.
   defaults: {
-    text: "there should be some data around here" + celsius
+    starttext: "there should be some data around here"
   },
 
-  getData() {
 
+
+  start: function () {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://10.10.10.166", false);
     xhttp.send()
     var celsius = xhttp.responseText;
     //document.getElementById("test").innerHTML = "bruh shit works " + celsius;
-  }
+  
+
+  },
+  getDom: function() {
+    var element = document.createElement("div")
+  element.className = "myContent"
+  element.innerHTML = celsius + this.config.starttext
+  return element
+
+  },
+  notificationReceived: function() {
+
+  },
+  socketNotificationReceived: function() {
+
+  },
+
+
 });
