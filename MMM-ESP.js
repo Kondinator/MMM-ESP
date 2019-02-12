22707207db8536b24837edd5f52aac030325c6ad
Kondinator <Kondiersej@live.dk>
2019-02-12 13:38:09 +0100

    gyeet

--- MMM-ESP.js
+++ MMM-ESP.js
@@ -16,10 +16,16 @@ Module.register("MMM-ESP", {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://10.10.10.166", false);
     xhttp.send()
-    var celsius = xhttp.responseText;
+
+    var celsius5 = celsius4;
+    var celsius4 = celsius3;
+    var celsius3 = celsius2;
+    var celsius2 = celsius1;
+    var celsius1 = xhttp.responseText;
+  
     var element = document.createElement("div")
     element.className = "myContent"
-    element.innerHTML = this.config.firsttext + " " + celsius + " " + this.config.lasttext
+    element.innerHTML = this.config.firsttext + " " + celsius1 + " " + this.config.lasttext
     return element
 
   },
