eb4015ae73dfd8fbebfae65d6d79127fb6be8d19
Kondinator <Kondiersej@live.dk>
2019-02-12 09:54:47 +0100

    sa

--- MMM-ESP.js
+++ MMM-ESP.js
@@ -8,31 +8,12 @@ Module.register("MMM-ESP", {
     text: "there should be some data around here" + celsius
   },
 
-  setPosition: function(pos) {
-		//Add css to body depending on the set position for notifications
-		var sheet = document.createElement("style");
-		if (pos === "center") {sheet.innerHTML = ".ns-box {margin-left: auto; margin-right: auto;text-align: center;}";}
-		if (pos === "right") {sheet.innerHTML = ".ns-box {margin-left: auto;text-align: right;}";}
-		if (pos === "left") {sheet.innerHTML = ".ns-box {margin-right: auto;text-align: left;}";}
-    document.body.appendChild(sheet);
-    
-  },
-
-  
-
-  // Override dom generator.
-	getDom: function() {
-		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.text;
-		return wrapper;
-	},
-
   getData() {
 
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://10.10.10.166", false);
     xhttp.send()
     var celsius = xhttp.responseText;
-    document.getElementById("test").innerHTML = "bruh shit works " + celsius;
+    //document.getElementById("test").innerHTML = "bruh shit works " + celsius;
   }
 });
