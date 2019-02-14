2a29efe06c990b559a1a114c2a681a88efd731b2
Kondinator <Kondiersej@live.dk>
2019-02-14 11:24:58 +0100

    plsyeet

--- MMM-ESP.js
+++ MMM-ESP.js
@@ -14,64 +14,35 @@ Module.register("MMM-ESP", {
 
   getDom: function () {
 
-    var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.text;
+    var chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      theme: "light2",
+      title:{
+        text: "Simple Line Chart"
+      },
+      axisY:{
+        includeZero: false
+      },
+      data: [{        
+        type: "line",       
+        dataPoints: [
+          { y: 450 },
+          { y: 414},
+          { y: 520, indexLabel: "highest",markerColor: "red", markerType: "triangle" },
+          { y: 460 },
+          { y: 450 },
+          { y: 500 },
+          { y: 480 },
+          { y: 480 },
+          { y: 410 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
+          { y: 500 },
+          { y: 480 },
+          { y: 510 }
+        ]
+      }]
+    });
+    chart.render();
     
-    
-    var div = document.createElement('div');
-    //div.style.height = '300px';
-    //div.style.width = '100%';
-    //div.style.display = 'none';
-    div.setAttribute('id', 'chartContainer'); // and make sure myclass has some styles in css
-    document.body.appendChild(div);
-
-    var celsius = ["0", "0", "0", "0", "0", "0"]
-
-    function myloop() {
-
-      celsius.shift()
-
-      var xhttp = new XMLHttpRequest();
-      xhttp.open("GET", "http://10.10.10.166", false);
-      xhttp.send()
-
-      celsius.push(xhttp.responseText)
-      console.log(celsius)
-
-      setTimeout(myloop, 20000)
-
-      var chart = new CanvasJS.Chart("chart-Container", {
-
-        toolTip:{
-        show: false,
-        animationEnabled: false,
-        theme: "light2",
-        },
-        title: {
-          text: "Simple Line Chart"
-        },
-        axisY: {
-          includeZero: true
-        },
-        data: [{
-          type: "line",
-          dataPoints: [
-
-            { y: Number(celsius[0]) },
-            { y: Number(celsius[1]) },
-            { y: Number(celsius[2]) },
-            { y: Number(celsius[3]) },
-            { y: Number(celsius[4]) },
-            { y: Number(celsius[5]) },
-          ]
-        }]
-      });
-      chart.render();
     }
-    myloop()
-    return wrapper;
 
-
-
-  }
 });
