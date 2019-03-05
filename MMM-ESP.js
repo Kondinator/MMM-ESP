641355f6e9f09d626ca925165b2af95d7643413a
Kondinator <Kondiersej@live.dk>
2019-03-05 12:58:13 +0100

    Revert "Initial commit"
    This reverts commit f610e79bb7a6308be924f00ffad33a7a66471a68.

---
+++ MMM-ESP.js
@@ -0,0 +1,147 @@
+//MMM-ESP.js:
+Module.register("MMM-ESP", {
+  // Default module config.
+  defaults: {
+    firsttext: "there should be some data around here",
+  },
+
+  getScripts: function () {
+    return [
+      'https://cdnjs.cloudflare.com/ajax/libs/echarts/4.1.0/echarts-en.common.min.js'
+
+    ]
+  },
+
+  getDom: function () {
+
+    var graphDiv = document.createElement('div');
+    graphDiv.setAttribute("id", "main");
+    graphDiv.style.width = '55%';
+    graphDiv.style.height = '40%';
+    document.body.appendChild(graphDiv);
+
+    var celsius = new Array(6).fill(null);
+    var tidspunkt = new Array(6).fill(0);
+
+    function myloop() {
+      celsius.shift()
+      tidspunkt.shift()
+
+      function addZero(i) {
+        if (i < 10) {
+          i = "0" + i;
+        }
+        return i;
+      }
+
+      function round5u(x)
+            {
+                return Math.ceil(x/5)*5;
+            }
+    
+            function round5d(x)
+            {
+                return Math.floor(x/5)*5;
+            }
+
+      var lowest = (Math.round(Math.min(...celsius)));
+      var highest = (Math.round(Math.max(...celsius)));
+      var now = new Date();
+      var m = addZero(now.getMinutes());
+      var h = addZero(now.getHours());
+      var t = h + ":" + m;
+      var setTime = String(t);
+      console.log(setTime + " " + h + " " + m)
+
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", "http://10.10.10.166", false);
+      xhttp.send()
+      celsius.push(Number(xhttp.responseText))
+      tidspunkt.push(setTime)
+      setTimeout(myloop, 600000)
+
+      // based on prepared DOM, initialize echarts instance
+      var myChart = echarts.init(document.getElementById('main'));
+
+      // specify chart configuration item and data
+      var option = {
+
+        title: {
+          text: 'Rum temperatur',
+          textStyle: {
+            color: '#fff',
+            fontSize: 30,
+            textShadowBlur: 2,
+            textShadowColor: '#000',
+            textShadowOffsetX: 0,
+            textShadowOffsetY: 1,
+            textBorderColor: '#333',
+            textBorderWidth: 2
+          },
+        },
+
+        tooltip: {
+        },
+
+        xAxis: { //bunden
+          type: 'category',
+
+          axisTick: {
+            //alignWithLabel: true,
+          },
+
+          axisLabel: {
+            //align: 'center',
+            color: 'black',
+            fontSize: 15,
+            textShadowBlur: 1,
+            textShadowColor: 'white',
+            textShadowOffsetX: 1,
+            textShadowOffsetY: 1,
+            //textBorderColor: '#333',
+            //textBorderWidth: '2',
+            //showMinLabel: true,
+            //showMaxLabel: true,
+          },
+
+          data: tidspunkt
+        },
+        yAxis: { //siden
+          min: round5d(lowest),
+          max: round5u(highest),
+          type: 'value',
+
+          axisLabel: {
+            show: true,
+            color: 'black',
+            fontSize: 15,
+            textShadowBlur: 2,
+            textShadowColor: 'white',
+            textShadowOffsetX: 0,
+            textShadowOffsetY: 1,
+            //textBorderColor: 'white', //bugged
+            //textBorderWidth: '2', //bugged
+            //showMinLabel: true,
+            //showMaxLabel: true,
+          },
+        },
+
+        series: [{
+          name: 'Temperatur',
+          type: 'line',
+          smooth: true,
+          lineStyle: {
+            width: 5,
+          },
+
+          data: celsius
+        }]
+      };
+
+      myChart.setOption(option);
+    }
+    myloop()
+    return graphDiv;
+  }
+
+});
